196283a23d5f7a76b603a6804840bc42bfa5575d
noemirtil <mirtil.noe@gmail.com>
2025-08-15 18:26:08 +0200

    Mobile navigation done

---
+++ js/nav.js
@@ -0,0 +1,79 @@
+// Mobile navigation
+const toggleMenu = () => {
+    document.getElementById("nav-mobile").classList.toggle("show-mob");
+    document.getElementById("shadow").classList.toggle("show-mob");
+    document.getElementById("burger").classList.toggle("burger--open");
+}
+
+const menuItems = {
+    'hero': ['hero-mob', 'hero-desk'],
+    'cabinet': ['cabinet-mob', 'cabinet-desk'],
+    'domaines': ['domaines-mob', 'domaines-desk'],
+    'honoraires': ['honoraires-mob', 'honoraires-desk'],
+    'contact': ['contact-mob', 'contact-desk']
+};
+
+const updateMenuStatus = (currentPage) => {
+    Object.keys(menuItems).forEach(page => {
+        menuItems[page].forEach(item => {
+            if (page === currentPage) {
+                document.getElementById(item).classList.add("current-page");
+                document.getElementById(item).classList.remove("current-not");
+            } else {
+                document.getElementById(item).classList.add("current-not");
+                document.getElementById(item).classList.remove("current-page");
+            }
+        });
+    });
+}
+
+window.addEventListener("load", (event) => {
+    event.preventDefault();
+    document.getElementById("burger").addEventListener('click', () => { toggleMenu(); });
+    document.getElementById("nav-mobile").addEventListener('click', (e) => {
+        let target = e.target;
+        switch (target.id) {
+            case 'hero-mob':
+                updateMenuStatus('hero');
+                break;
+            case 'cabinet-mob':
+                updateMenuStatus('cabinet');
+                break;
+            case 'domaines-mob':
+                updateMenuStatus('domaines');
+                break;
+            case 'honoraires-mob':
+                updateMenuStatus('honoraires');
+                break;
+            case 'contact-mob':
+                updateMenuStatus('contact');
+                break;
+        }
+        toggleMenu();
+    });
+    // document.getElementById("menu-section").addEventListener('click', (e) => {
+    //     let target = e.target;
+    //     switch (target.id) {
+    //         case 'home-desk':
+    //             document.getElementById("homegrid").style.display = "grid";
+    //             document.getElementById("pro-page").style.display = "none";
+    //             window.scrollTo({ top: 0, behavior: "smooth" });
+    //             updateMenuStatus('home');
+    //             break;
+    //         case 'pro-desk':
+    //             document.getElementById("homegrid").style.display = "none";
+    //             document.getElementById("pro-page").style.display = "flex";
+    //             window.scrollTo({ top: 0, behavior: "smooth" });
+    //             updateMenuStatus('pro');
+    //             break;
+    //         case 'contact-desk':
+    //             window.scrollTo({
+    //                 top: document.body.scrollHeight,
+    //                 behavior: 'smooth'
+    //             });
+    //             updateMenuStatus('contact');
+    //             break;
+    //     }
+    // });
+});
+
