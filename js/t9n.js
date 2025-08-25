a6015587c9aec1a08079c15d8db4d3c2e74fb0c8
noemirtil <mirtil.noe@gmail.com>
2025-08-25 13:28:28 +0200

    Translated to spanish

--- js/t9n.js
+++ js/t9n.js
@@ -1,63 +1,40 @@
 //#MARK: Translations
 const translations = {
     esp: {
-        "subtitle": "Abogada en París y Barcelona",
-        "accueil-mob": "Inicio",
-        "cabinet-mob": "El despacho",
-        "domaines-mob": "Servivios",
-        "engagement-mob": "Mi compromiso",
-        "honoraires-mob": "Honorarios",
-        "contact-mob": "Contacto",
-        // "home-desk": "Inicio",
-        // "contact-desk": "Contacto",
-        // "contact-title": "Contacto",
-        // "nombre-label": "Nombre",
-        // "pronombre-label": "Apellido",
-        // "email-label": "Email",
-        // "mensaje-label": "Mensaje",
+        // "subtitle": "Abogada en París y Barcelona",
+        // "accueil-mob": "Inicio",
+        // "cabinet-mob": "El despacho",
+        // "domaines-mob": "Servivios",
+        // "engagement-mob": "Mi compromiso",
+        // "honoraires-mob": "Honorarios",
+        // "contact-mob": "Contacto",
         "submit": `Enviar&nbsp;&nbsp;&nbsp;<img class="arrow" src="img/arrow.svg" class= "reverse" alt="Icono enviar" >`,
         "result1": "<p>Enviando tu mensaje...</p>",
         "result2": `<p>¡Gracias por tu mensaje!\n\nTe responderé en cuanto pueda</p>`,
         "result3": `<p>Algo no ha salido bien, lo siento</p>`,
     },
     fra: {
-        "subtitle": "Avocate à Paris et Barcelone",
-        "accueil-mob": "Accueil",
-        "cabinet-mob": "Le cabinet",
-        "domaines-mob": "Domaines<br>d'intervention",
-        "engagement-mob": "Mon engagement",
-        "honoraires-mob": "Honoraires",
-        "contact-mob": "Contact",
+        // "subtitle": "Avocate à Paris et Barcelone",
+        // "accueil-mob": "Accueil",
+        // "cabinet-mob": "Le cabinet",
+        // "domaines-mob": "Domaines<br>d'intervention",
+        // "engagement-mob": "Mon engagement",
+        // "honoraires-mob": "Honoraires",
         // "contact-mob": "Contact",
-        // "home-desk": "Accueil",
-        // "contact-desk": "Contact",
-        // "contact-title": "Contact",
-        // "nombre-label": "Prénom",
-        // "pronombre-label": "Nom",
-        // "email-label": "E-mail",
-        // "mensaje-label": "Message",
-        "submit": `Envoyer&nbsp;&nbsp;&nbsp;<img class="arrow" src="img/arrow.svg" class= "reverse" alt="Icono enviar" >`,
+        // "submit": `Envoyer&nbsp;&nbsp;&nbsp;<img class="arrow" src="img/arrow.svg" class= "reverse" alt="Icono enviar" >`,
         "result1": "<p>Envoi du message en cours...</p>",
         "result2": `<p>Merci pour ton message!\n\nJe te répondrai\ndès que possible</p>`,
         "result3": "<p>Quelque chose n'a pas fonctionné, désolée...</p>",
     },
     eng: {
-        "subtitle": "Lawyer in Paris and Barcelona",
-        "accueil-mob": "Home",
-        "cabinet-mob": "The firm",
-        "domaines-mob": "Services",
-        "engagement-mob": "My commitment",
-        "honoraires-mob": "Fees",
-        "contact-mob": "Contact",
+        // "subtitle": "Lawyer in Paris and Barcelona",
+        // "accueil-mob": "Home",
+        // "cabinet-mob": "The firm",
+        // "domaines-mob": "Services",
+        // "engagement-mob": "My commitment",
+        // "honoraires-mob": "Fees",
         // "contact-mob": "Contact",
-        // "home-desk": "Home",
-        // "contact-desk": "Contact",
-        // "contact-title": "Contact",
-        // "nombre-label": "First name",
-        // "pronombre-label": "Last name",
-        // "email-label": "E-mail",
-        // "mensaje-label": "Message",
-        "submit": `Send&nbsp;&nbsp;&nbsp;<img class="arrow" src="img/arrow.svg" class= "reverse" alt="Icono enviar" >`,
+        // "submit": `Send&nbsp;&nbsp;&nbsp;<img class="arrow" src="img/arrow.svg" class= "reverse" alt="Icono enviar" >`,
         "result1": "<p>Sending your message...</p>",
         "result2": `<p>Thanks for your message!\n\nI'll get in touch asap</p>`,
         "result3": "<p>Something went wrong, sorry...</p>",
@@ -70,21 +47,14 @@ const translate = () => {
         document.getElementById(value).innerHTML = translations[language][value];
     }
     // elements to translate
-    t9n("subtitle");
-    t9n("accueil-mob");
-    t9n("cabinet-mob");
-    t9n("domaines-mob");
-    t9n("engagement-mob");
-    t9n("honoraires-mob");
-    t9n("contact-mob");
+    // t9n("subtitle");
+    // t9n("accueil-mob");
+    // t9n("cabinet-mob");
+    // t9n("domaines-mob");
+    // t9n("engagement-mob");
+    // t9n("honoraires-mob");
     // t9n("contact-mob");
-    // t9n("home-desk");
-    // t9n("contact-title");
-    // t9n("nombre-label");
-    // t9n("pronombre-label");
-    // t9n("email-label");
-    // t9n("mensaje-label");
-    t9n("submit");
+    // t9n("submit");
 }
 
 
@@ -103,19 +73,19 @@ window.addEventListener("load", (event) => {
                 language = "esp";
                 translate();
                 updateLangStatus('esp');
-                // toggleMenu();
+                localStorage.setItem('langPref', 'esp');
                 break;
             case 'fra-mob':
                 language = "fra";
                 translate();
                 updateLangStatus('fra');
-                // toggleMenu();
+                localStorage.setItem('langPref', 'fra');
                 break;
             case 'eng-mob':
                 language = "eng";
                 translate();
                 updateLangStatus('eng');
-                // toggleMenu();
+                localStorage.setItem('langPref', 'eng');
                 break;
         }
     });
